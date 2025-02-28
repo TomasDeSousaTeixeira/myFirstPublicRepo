459d4e75b3d64f0f052113f4e1d634b44096e1fc
TomasTeixeira <tomas_teixeira_@hotmail.com>
2025-02-28 12:46:33 +0000

    move database variables to .env.local file

--- clock-it-API/db/connection.js
+++ clock-it-API/db/connection.js
@@ -2,13 +2,13 @@ import postgres from 'postgres';
 import dotenv from "dotenv";
 
 dotenv.config({ path: ".env.local" });
-const DB_CONNECTION_PASSWORD = process.env.DB_CONNECTION_PASSWORD;
+
 const sql = postgres({
-    host: 'localhost',
-    port: 5432,
-    database: 'postgres',
-    username: 'postgres',
-    password: DB_CONNECTION_PASSWORD, // .env.local variable
-  });
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
   
 export default sql;
